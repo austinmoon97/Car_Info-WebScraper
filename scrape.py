c4c7ee95846081e6e42c0afb1882f228bb986698
Austin Moon <austinmoon@Austins-MBP-3.lan1>
2020-01-22 20:07:01 -0800

    Scrapes all trims of car properly.  Fixed to scrape price/drivetrain info from html containing all trims instead of same html page as the other info.  Specifications are listed in different order for different cars, so program updated to hunt through all specification info instead of through specified indices.  All info is writing properly to csv file.

--- scrape.py
+++ scrape.py
@@ -1,25 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+import re
 
-car_info = {
-    "Price": "no info",
-    "Transmission": "no info",
-    "Horsepower": "no info",
-    "Torque": "no info",
-    "Engine liters": "no info",
-    "Cylinder configuration": "no info",
-    "Drivetrain": "no info",
-    "Combined MPG": "no info",
-    "Fuel economy city": "no info",
-    "Fuel economy highway": "no info",
-    "Recommended fuel": "no info",
-    "Fuel tank capacity": "no info",
-    "Front tires": "no info",
-    "Curb weight": "no info",
-    "Basic warranty (months/miles)": "no info",
-    "Powertrain warranty (months/miles)": "no info"
-}
 
+# PARAMETER(S):
+#     info_to_scrape (.TXT FILE) = contains desired info to be scraped
+#
+# FUNCTION: 
+#     parses text file by line, removes \n characters, returns list of info to scrape
+#
 def textfile_to_list(info_to_scrape):
     f = open(info_to_scrape, 'r')
     newlist = [item for item in f.readlines()]
@@ -28,19 +17,123 @@ def textfile_to_list(info_to_scrape):
     for i in range(len(newlist)-1):
         newlist[i] = newlist[i][:-1]
 
-    #for l in newlist:
-        #print(l)
     return newlist
 
+# PARAMETER(S): 
+#     trim (STRING) = trim level of car to be scraped
+#     trim_url (STRING) = url to trim info
+#     car_info (DICT) = holds scraped info
+#     all_trims_soup (BS4 OBJ) = parsable html of https://www.cars.com/research/make-model-year/trims/
+#
+# FUNCTION:
+#     scrapes info for a specific trim, returns populated dictionary
+#
+def scrape_trim(trim, trim_url, car_info, all_trims_soup):
+
+    #open url to specific trim
+    uClient = urlopen("https://www.cars.com" + trim_url)
+    page_html = uClient.read()
+    uClient.close()
+
+    #construct beautiful object
+    page_soup = soup(page_html, "html.parser")
+
+    #find overview info
+    overview_container = page_soup.find("table", {"class": "overview-table"})
+    overview_items = overview_container.tbody.findAll("tr")
+
+    for item in overview_items:
+        if item.td.text in car_info:
+            if len(item.findAll("td")[1].text) > 0:
+                car_info[item.td.text] = item.findAll("td")[1].text
+
+    #find specifications container
+    specs_container = page_soup.find("section", {"id": "specifications-section"})
+
+    #divide sections of specifications
+    specs_sections = specs_container.div.findAll("table", {"id": "specs-accordion-table"})
+
+    ########################################################
+
+
+    for section in specs_sections:
+        specs = section.find("tbody").findAll("tr")
+        for spec in specs:
+            if spec.td.text in car_info:
+                car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
+
+
+    #######################################################
+    """
+    #find engine specs
+    engine_specs = specs_sections[3].find("tbody").findAll("tr")
+    
+    for spec in engine_specs:
+        if spec.td.text in car_info:
+            car_info[spec.td.text] = spec.findAll("td")[1].text
+
+    #find suspension/handling specs
+    suspension_specs = specs_sections[6].find("tbody").findAll("tr")
+
+    for spec in suspension_specs:
+        if spec.td.text in car_info:
+            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
+
+    #find standard specs
+    standard_specs = specs_sections[12].find("tbody").findAll("tr")
+
+    for spec in standard_specs:
+        if spec.td.text in car_info:
+            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
+
+    #find warranty specs
+    warranty_specs = specs_sections[13].find("tbody").findAll("tr")
+
+    for spec in warranty_specs:
+        if spec.td.text in car_info:
+            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
+    """
+
+    #find price and drivetrain (not available on the same html page)
+    #use bs4 object corresponding to https://www.cars.com/research/make-model-year/trims/
+    trim_containers = all_trims_soup.findAll("div", {"class":"cui-accordion-section"})[:-3]
+
+    #find container of the correct trim
+    for container in trim_containers:
+        if container.label.h3.text[6:] == trim:
+            
+            #find divs containing price and drivetrain
+            target_divs = container.findAll("div", {"class":"cell cell-bg"})
+
+            #extract price/drivetrain
+            price = re.sub("[a-z A-Z]", "", target_divs[0].text)
+            drivetrain = target_divs[1].text
+
+            car_info["Price"] = price
+            car_info["Drivetrain"] = drivetrain
+            
+
+    for x,y in car_info.items():
+        print(x + ": " + y)
+
+    return car_info
+
+# PARAMETER(S):
+#     car_make (STRING) = make of car
+#     car_model (STRING) = model of car
+#     car_year (STRING) = year of car
+#     info_to_scrape (.TXT FILE or LIST) = what info to scrape
+#
+# FUNCTION:
+#     Initial function to be called.
+#     Creates and saves csv file with car info in current directory
+#
 def scrape_car(car_make, car_model, car_year, info_to_scrape):
 
     if type(info_to_scrape) is list:
          print("LIST!")
     else:
         info_to_scrape = textfile_to_list(info_to_scrape)
-
-    
-    car_info = {info: "no info" for info in info_to_scrape}
 
 
     #construct car url from make, model, year parameters
@@ -71,88 +164,31 @@ def scrape_car(car_make, car_model, car_year, info_to_scrape):
     filename = f"{car_make}_{car_model}_{car_year}_info.csv"
     car_file = open(filename, "w")
 
-    #fill in header
+    #fill in csv header
     car_file.write("Trims")
-    for item in car_info:
+    for item in info_to_scrape:
         car_file.write("," + item)
     car_file.write("\n")
 
+    #fetch info for every trim level
+    for trim in trim_urls:
+        #initialize dictionary of car info to scrape
+        car_info = {info: "no info" for info in info_to_scrape}
+        
+        #populates car_info
+        trim_info = scrape_trim(trim, trim_urls[trim], car_info, trim_page_soup)
 
-    #for trim in trim_urls:
-        #trim_info = scrape_trim(trim, car_info, car_file)
-        #print(trim_info)
+        #write trim info to csv
+        car_file.write(trim)
+        for info in car_info.values():
+            #remove commas from info, confuses with csv delimiter
+            if "," in info:
+                info = info.replace(",", "")
+            car_file.write("," + info)
+        car_file.write("\n")
 
-
-    #open url to specific trim
-    uClient2 = urlopen("https://www.cars.com" + trim_urls["L"])
-    page_html2 = uClient2.read()
-    uClient2.close()
-
-    page_soup2 = soup(page_html2, "html.parser")
-
-    #find price
-    price_tag = page_soup2.find("div", {"class": "specs-price__value"})
-    price = price_tag.text
-    car_info["Price"] = price
-
-    #find overview info
-    overview_container = page_soup2.find("table", {"class": "overview-table"})
-    overview_items = overview_container.tbody.findAll("tr")
-
-    for item in overview_items:
-        if item.td.text in car_info:
-            car_info[item.td.text] = item.findAll("td")[1].text
-
-    #find specifications container
-    specs_container = page_soup2.find("section", {"id": "specifications-section"})
-
-    #divide sections of specifications
-    specs_sections = specs_container.div.findAll("table", {"id": "specs-accordion-table"})
-
-    #find engine specs
-    engine_specs = specs_sections[3].find("tbody").findAll("tr")
-    
-    for spec in engine_specs:
-        if spec.td.text in car_info:
-            car_info[spec.td.text] = spec.findAll("td")[1].text
-
-    #find suspension/handling specs
-    suspension_specs = specs_sections[6].find("tbody").findAll("tr")
-
-    for spec in suspension_specs:
-        if spec.td.text in car_info:
-            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
-
-    #find standard specs
-    standard_specs = specs_sections[12].find("tbody").findAll("tr")
-
-    for spec in standard_specs:
-        if spec.td.text in car_info:
-            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
-
-    #find warranty specs
-    warranty_specs = specs_sections[13].find("tbody").findAll("tr")
-
-    for spec in warranty_specs:
-        if spec.td.text in car_info:
-            car_info[spec.td.text] = spec.find("td",{"class":"data"}).text
-
-    for x,y in car_info.items():
-        print(x + ": " + y)
+        print(trim)
+        print(trim_info)
 
 
-
-
-
-
-    
-
-    car_file.close()
-
-    #uClient2 = urlopen("https://www.cars.com" + new_url)
-    #page_html2 = uClient2.read()
-    #uClient.close()
-
-    #page_soup2 = soup(page_html2, "html.parser")
-
-    #print(page_soup2.h1)
+    car_file.close()        #close csv file
